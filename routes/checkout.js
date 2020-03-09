fd4c922fe24dc5a0ac7390981847b4ab2c6d75d1
sam-grierson <60353903+sam-grierson@users.noreply.github.com>
2020-03-09 19:51:43 +0000

    adding post request to checkout

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 const router = express.Router();
 
 const Cart = require("../models/cart")
@@ -6,11 +7,48 @@ const Cart = require("../models/cart")
 router.get("/", (req, res) => {
   let cart = new Cart(req.session.cart ? req.session.cart : {});
 
-
   res.render("checkout", {
     cartCount: cart.totalQty,
     cartEmpty: cart.totalQty,
     loggedIn: req.session.loggedin,
+    total: cart.totalPrice,
+    paymentSub: null
+  });
+});
+
+// let db = new sqlite3.Database("db/database.db");
+// let sql = `INSERT INTO user(username,firstname,surname,email,phoneNo,address,cardName,cardNo,expMon,expYr) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`
+// db.run(sql, [username,firstName,surname,email,phoneNo,address,cardName,cardNo,expMon,expYr], (err) => {
+//   if (err) {
+//     return console.error(err.message);
+//   }
+// });
+
+// Guest checkout payments go into the void
+router.post("/", (req, res) => {
+  let cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  let firstName = req.body.firstname;
+  let surname = req.body.secondname;
+  let email = req.body.email;
+  let phoneNo = req.body.phoneno;
+  let address = req.body.address;
+  let cardName = req.body.firstname;
+  let cardNo = req.body.firstname;
+  let expMon = req.body.expmon;
+  let expYr = req.body.expyr;
+
+  // need to add input validation
+
+  cart.clearCart()
+  req.session.cart = cart
+  
+  res.render("checkout", {
+    cartCount: cart.totalQty,
+    cartEmpty: cart.totalQty,
+    loggedIn: req.session.loggedin,
+    total: cart.totalPrice,
+    paymentSub: "Payment Successful!"
   });
 });
 
