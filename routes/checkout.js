7a43aed5be5440e1787329afb96f52dcd4e9bb23
0xMo <47088174+0xMo@users.noreply.github.com>
2020-03-09 19:37:18 +0000

    add a function to check if the user is logged in

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,3 +1,4 @@
+const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const router = express.Router();
 
@@ -5,12 +6,22 @@ const Cart = require("../models/cart")
 
 router.get("/", (req, res) => {
   let cart = new Cart(req.session.cart ? req.session.cart : {});
+  let session = req.session
+
+  function getUser(session) {
+    if (session.loggedin === true) {
+      return req.session.username;
+    } else {
+      return false;
+    }
+  }
 
 
   res.render("checkout", {
     cartCount: cart.totalQty,
     cartEmpty: cart.totalQty,
     loggedIn: req.session.loggedin,
+    name: getUser(session)
   });
 });
 
